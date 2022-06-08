1834e171becc0bd3eca09fcdecb7f6f945e069ee
Gonzalo <gonfarias6@gmail.com>
2022-06-08 19:38:54 -0300

    initial commit

--- public/js/countdown.js
+++ public/js/countdown.js
@@ -6,10 +6,10 @@ simplyCountdown('#cuenta', {
 	minutes: 0, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
-		days: 'Día',
-		hours: 'Hora',
-		minutes: 'Minuto',
-		seconds: 'Segundo',
+		days: 'Days',
+		hours: 'Hours',
+		minutes: 'Minutes',
+		seconds: 'Seconds',
 		pluralLetter: 's'
 	},
 	plural: true, //use plurals
