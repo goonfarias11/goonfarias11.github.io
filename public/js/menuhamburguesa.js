7420a6e9b84dabf43735ba7c2b6c4908c4d3bde8
goonfarias11 <gonfarias6@gmail.com>
2022-11-22 12:37:48 -0300

    commit add 22/11

---
+++ public/js/menuhamburguesa.js
@@ -0,0 +1,10 @@
+const iconoMenu = document.querySelector('#icono-menu'),
+      menu = document.querySelector('#menu');
+
+iconoMenu.addEventListener('click', (e) => {
+
+    // Alternamos estilos para el menú y body
+    menu.classList.toggle('active');
+    const opacity = document.body.classList.toggle('opacity');
+    opacity.body.style
+})
